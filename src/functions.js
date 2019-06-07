84648878a1ae30e91b619fb675a40f4d13e72718
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-06-07 11:46:30 -0700

    haven't commited in a bit. Whoops. Have skeleton for API requests now

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,17 @@
-export function add(a, b) {
-    return a + b;
+export class conditionSearch() {
+    getDoctorsByCondition(condition) {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            let url = 'https://api.betterdoctor.com/2016-03-01/conditions?fields=${conditoin}&limit=10&user_key={process.env.apiKEY}';
+            request.onload = function () {
+                if (this.status === 200) {
+                    resolve(request.response);
+                } else {
+                    reject(Error(request.statusText));
+                }
+            };
+            request.open("GET", url, true);
+            request.send();
+        });
+    }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-//require('exports-loader?file!./bootstrap/js/dist/.js')
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
-//$(document).ready(function() {
+import { conditionSearch } from './function.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
-//});
+$(document).ready(function () {
+	$('#medicalCondition').click(function () {
+		let condition = $('#condition').val();
+		$('#condition').val("");
+
+		let conditionSearch = new conditionSearch();
+		let promise = conditionSearch.getDoctorsByCondition(condition);
+
+		promise.then(function (response) {
+			let body = JSON.parse(response);
+			$('.displayName').text(`The Doctor, Dr. ${body.data.firstName} `);
+		}, function (error) {
+			$('.error').text(`There was an error processing your request: ${error.message}`);
+		});
+	});
+
+});
